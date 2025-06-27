768c22006cebedd319b97485a28a898b2a86520e
Artyom <162967923+arslanberatt@users.noreply.github.com>
2025-06-27 21:10:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(__dirname));
+app.use("/api/uploads", express.static(__dirname));
 
 app.use(cors(corsOptions));
 app.use("/api", router);
