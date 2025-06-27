e01360b549350eb5944dafbf44089a0868935950
Artyom <162967923+arslanberatt@users.noreply.github.com>
2025-06-27 12:48:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
+app.set("trust proxy", 1);
 app.use("/api", apiLimiter);
 
 app.use((req, res, next) => {
