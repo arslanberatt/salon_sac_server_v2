f77a7fbf3be9a84887ac36872b4da1b346eeddb0
Artyom <162967923+arslanberatt@users.noreply.github.com>
2025-07-02 14:28:33 +0300

    bcryptjs geçişi tamamlandı, login ve reset şifre stabil hale getirildi

--- src/helpers/corsOptions.js
+++ src/helpers/corsOptions.js
@@ -1,14 +1,10 @@
-const whiteList = [
-  process.env.SERVER,
-  "https://salonsacserverv2-production.up.railway.app" // ← sondaki slash kaldırıldı
-];
+const whiteList = [process.env.SERVER];
 
 const corsOptions = (req, callback) => {
   const origin = req.header("Origin");
   const isAllowed = whiteList.includes(origin);
 
-  const corsOpt = { origin: isAllowed };
-  callback(null, corsOpt);
+  callback(null, { origin: isAllowed });
 };
 
 module.exports = corsOptions;
