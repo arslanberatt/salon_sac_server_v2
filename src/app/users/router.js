151addf4c25b418efdb4beaf100ee37383708216
Artyom <162967923+arslanberatt@users.noreply.github.com>
2025-07-02 12:41:33 +0300

    bcryptjs geçişi tamamlandı, login ve reset şifre stabil hale getirildi

--- src/app/users/router.js
+++ src/app/users/router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const upload = require("../../middlewares/lib/upload");
 const { tokenCheck, adminCheck } = require("../../middlewares/auth");
-const { getMe, updateMe, updateUserByAdmin } = require("./controller");
+const { getMe, updateMe, updateUserByAdmin, getActiveUsers, getPassiveUsers } = require("./controller");
 
 router.get("/me", tokenCheck, getMe);
 router.get("/active", tokenCheck, adminCheck, getActiveUsers);
