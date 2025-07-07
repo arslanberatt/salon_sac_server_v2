093027f5d0d01c09db5d39f08767a9f08ab38672
Artyom <162967923+arslanberatt@users.noreply.github.com>
2025-07-07 16:28:22 +0300

    randevulardaki markasdone fonksiyonu düzeltildi

--- src/app/users/router.js
+++ src/app/users/router.js
@@ -1,16 +1,10 @@
 const router = require("express").Router();
 const upload = require("../../middlewares/lib/upload");
-const { tokenCheck, adminCheck, modCheck } = require("../../middlewares/auth");
-const {
-  getMe,
-  updateMe,
-  updateUserByAdmin,
-  getActiveUsers,
-  getPassiveUsers,
-} = require("./controller");
+const { tokenCheck, adminCheck } = require("../../middlewares/auth");
+const { getMe, updateMe, updateUserByAdmin, getActiveUsers, getPassiveUsers } = require("./controller");
 
 router.get("/me", tokenCheck, getMe);
-router.get("/active", tokenCheck, adminCheck, getActiveUsers);
+router.get("/active", tokenCheck, getActiveUsers);
 router.get("/passive", tokenCheck, adminCheck, getPassiveUsers);
 router.put("/update-user", tokenCheck, upload.single("avatar"), updateMe);
 router.put("/users/:id", tokenCheck, adminCheck, updateUserByAdmin);
