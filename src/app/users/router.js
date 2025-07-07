22ec76db514a0a043b25491e8c33c53439136865
Artyom <162967923+arslanberatt@users.noreply.github.com>
2025-07-07 15:47:05 +0300

    randevulardaki markasdone fonksiyonu düzeltildi

--- src/app/users/router.js
+++ src/app/users/router.js
@@ -4,7 +4,7 @@ const { tokenCheck, adminCheck } = require("../../middlewares/auth");
 const { getMe, updateMe, updateUserByAdmin, getActiveUsers, getPassiveUsers } = require("./controller");
 
 router.get("/me", tokenCheck, getMe);
-router.get("/active", tokenCheck, adminCheck, getActiveUsers);
+router.get("/active", tokenCheck, getActiveUsers);
 router.get("/passive", tokenCheck, adminCheck, getPassiveUsers);
 router.put("/update-user", tokenCheck, upload.single("avatar"), updateMe);
 router.put("/users/:id", tokenCheck, adminCheck, updateUserByAdmin);
